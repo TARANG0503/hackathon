124dee50482b06f3f2f396b284b0db5a7108249e
TARANG0503 <40140984+TARANG0503@users.noreply.github.com>
2019-10-11 03:53:39 +0530

    Update header.php

--- hackathon/header.php
+++ hackathon/header.php
@@ -7,16 +7,8 @@
     <title>Just A test</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-      <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
-      <!--Let browser know website is optimized for mobile-->
-      <link rel="stylesheet" href="Project.css">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    
     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
-    <script src="main.js"></script>
 </head>
 <body>
         
@@ -41,35 +33,7 @@
 
             </form>
         </div> -->
-        <section class="slider">
-            <ul class="slides">
-                    <li>
-                      <img src="https://images.indianexpress.com/2017/06/anganwari-1.jpg?w=759&h=422&imflag=true"> <!-- random image -->
-                    </li>
-                    <li>
-                      <img src="https://delhifoodbanking.org/wp-content/uploads/2019/05/Every-single-state-in-India-is-home-to-people-who-face-hunger-every-day.-.jpg"> <!-- random image -->
-                      <div class="caption left-align">
-                        <h2>Save Food,Save Life !</h2>
-                        <h4 class="light grey-text text-lighten-3 hide-on-small-only">World Food Day - 16 October</h4>
-                      </div>
-                    </li>
-                    <li>
-                      <img src="https://image.shutterstock.com/image-photo/assorted-indian-food-on-dark-260nw-573575512.jpg"> <!-- random image -->
-                      <div class="caption center-align">
-                        <h2>Wasting food is like </h2>
-                        <h4 class="light grey-text text-lighten-3 hide-on-small-only">stealing from the poors.</h4>
-                      </div>
-                    </li>
-                    <li>
-                      <img src="https://fssai.gov.in/upload/uploadfiles/images/share_food.png"> <!-- random image -->
-                    </li>
-                  </ul>
-     </section>
-
-<q><h5>'There are people in world so hungry , that God can't appear to them except in the form of bread.'</h5></q>
-<blockquote><h5><b>When I give food to the poor , they call me a saint.So ,if you want to feed a hungry please click the link given below!</b></h5></blockquote>
-<button class= "btn-large btn-modify button" id ="btn" >Send Help</button>
-     <div class="msgForm" id = "msgForm"></div>
+        
         <div class="wrapper">
             <button onclick="window.location.href='signup.php'" class="register">REGISTER</button>
         </div>
