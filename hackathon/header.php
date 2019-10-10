35bd38aa803f296d44ec06cf50feffa735fa1241
TARANG0503 <40140984+TARANG0503@users.noreply.github.com>
2019-10-11 04:13:48 +0530

    Update header.php

--- hackathon/header.php
+++ hackathon/header.php
@@ -5,15 +5,13 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Just A test</title>
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <!-- <script src="main.js"></script> -->
 </head>
 <body>
-        
         <div class="top-nav">
-                <img src="assets/logo.png" class="logo" >
+                <img src="assets/logo.png" alt="http://localhost/hackathon/"class="logo" >
 
             
             <div class="nav-container ">
@@ -24,6 +22,10 @@
                 </ul>
            </div>
         </div>
+        <q><h3>'There are people in world so hungry , that God can't appear to them except in the form of bread.'</h3></q>
+<blockquote><h1>When I give food to the poor , they call me a saint.So ,if you want to feed a hungry please click the link given below!</h1></blockquote>
+
+        <h2>For the cateres who wish to donate the leftovers of grand events and ceremonies to those who starve to death</h2>      
         <!-- <div>
             <form action="" method="post">
                 <input type="text" name="name" placeholder="Full Name">
@@ -33,9 +35,13 @@
 
             </form>
         </div> -->
-        
-        <div class="wrapper">
-            <button onclick="window.location.href='signup.php'" class="register">REGISTER</button>
+        <div class="form-container">
+            <div class="wrapper1">
+                <button onclick="window.location.href='signup.php'" class="register">REGISTER</button>
+            </div>
+            <div class="wrapper2">
+                <button onclick="window.location.href='help.php'" class="help">HELP</button>
+            </div>
         </div>
     
 </body>
