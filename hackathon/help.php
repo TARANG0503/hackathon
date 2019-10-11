1610b63ff05db068a8be04cd8cbfec114574b4b4
TARANG0503 <40140984+TARANG0503@users.noreply.github.com>
2019-10-11 09:29:37 +0530

    Update help.php

--- hackathon/help.php
+++ hackathon/help.php
@@ -19,6 +19,8 @@ require "header.php";
            <button class="help" type="submit" name="help-submit">Help</button>
         </form>  
       </div>  
+          <div class="mapouter"><div class="gmap_canvas"><iframe  width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=new%20delhi%20&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/nordvpn-coupon-code/">nordvpn coupon</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;margin:auto;padding:25px 0px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div> 
+
     </section>
   </div>
 </main>
